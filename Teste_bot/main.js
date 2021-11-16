d1cf888d701dcba1c5c66a9570c42c2ec3c80121
Citeli-py <matheus.citeli2001@gmail.com>
2021-11-16 17:10:57 -0300

    Caixas de id e token

--- Teste_bot/main.js
+++ Teste_bot/main.js
@@ -1,9 +1,11 @@
 
 function send()
 {
-    var msg = document.getElementById('input').value;
-    const BOT_TOKEN = "<TOKEN>";
-    const CHAT_ID = "<CHAT_ID>";
+    var msg = document.getElementById('msg').value;
+    const BOT_TOKEN = document.getElementById('token').value;
+    const CHAT_ID = document.getElementById('id').value;
+    //const BOT_TOKEN = "<TOKEN>";
+    //const CHAT_ID = "<CHAT_ID>";
     const Url = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage?chat_id=${CHAT_ID}&text=${msg}`
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open( "GET", Url, false );
