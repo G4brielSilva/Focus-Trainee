258add85301004db6cbba52461364ecf465877f6
Citeli-py <matheus.citeli2001@gmail.com>
2021-11-28 16:37:09 -0300

    .

--- Site/main.js
+++ Site/main.js
@@ -1,15 +1,31 @@
-var quantidade = [0]*10;
+var quantidade = [];
+var n = 40;
+for(i=0; i<n; i++)
+{
+    quantidade[i] = 0;
+}
 
 function add(id)
 {
-    quantidade[id] += 1;
+    quantidade[id] ++;
     document.getElementById(id).innerHTML = quantidade[id];
 }
 
 function sub(id)
 {
     if(quantidade[id] - 1 >= 0)
-        quantidade[id] -= 1;
+        quantidade[id] --;
 
     document.getElementById(id).innerHTML = quantidade[id];
+}
+
+function carrinho()
+{
+    for(i=0; i<n; i++)
+    {
+        if(quantidade[i] > 0)
+        {
+            console.log("Produto:",i," Quantidade: ", quantidade[i]);
+        }
+    }
 }
